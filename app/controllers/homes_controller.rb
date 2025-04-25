0cc8ef570efe472316ef08ca903db356a8189e8e
Lucas Sousa <ls442336@gmail.com>
2025-04-25 07:48:10 -0300

    Fix cache assignment

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < DashboardController
   def show
-    Rails.cache.fetch("ipinfo/#{request.remote_ip}", expires_in: 1.hour, skip_nil: true) do
-      @ipinfo = IpinfoApi.with_defaults.fetch_info(request.remote_ip)
+    @ipinfo = Rails.cache.fetch("ipinfo/#{request.remote_ip}", expires_in: 1.hour, skip_nil: true) do
+      IpinfoApi.with_defaults.fetch_info(request.remote_ip)
     end
   rescue IpinfoApi::Error => e
     Rails.logger.error "Failed to fetch IP info: #{e.message}"
